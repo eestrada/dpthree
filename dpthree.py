f4f329874d87d595c9463f69806d810b1ac78160
Ethan Estrada <ethan.estrada@gmail.com>
2015-03-01 15:05:41 -0700

    Move version agnostic code out of if statement

--- dpthree.py
+++ dpthree.py
@@ -1,29 +1,34 @@
-"""A module to deal with the differences between python2 and python3.
+"""Module to deal with the differences between Python 2 and Python 3 builtins.
 
-It is meant to be imported with a star import, like:
-    from py3_fixes import *
-When imported this way, it will override builtin functions that have different
-behaviour in python3. It also imports some functions that have the same
-functionality, but different names in python3. For instance, the unicode
-builtin in python2 is the same as the str builtin in python3. It is defined
+dpthree stands for "Duck Punch Three." This is because the builtins for the
+current module get overwritten via Duck Punching. This is also sometimes called
+Monkey Patching.
+
+The module is meant to be imported with a star import, like this:
+    from dpthree import *
+
+When imported this way, it will override builtin callables that have different
+behaviour in Python 3. It also imports some callables that have the same
+functionality, but different names in Python 3. For instance, the unicode
+builtin in Python 2 is the same as the str builtin in Python 3. It is defined
 here to help port old code. However, any names that have been removed in
-python3 use a DeprecationWarning (which will either be ignored, printed, or
+Python 3 use a DeprecationWarning (which will either be ignored, printed, or
 raised based on the warning level used).
 
 Removals to be made aware of:
 
-xrange has been removed in python3. Well, sort of; range is replaced with
-xrange, but with some increased functionality. For instance, in python3,
-range/wrange objects can be indexed into like a regular sequence.
+xrange has been removed in Python 3. Well, sort of; xrange is replaced with
+range, but with some increased functionality. For instance, in Python 3,
+range objects can be larger than sys.maxsize.
 
-unicode has been removed in python3. Well, again, sort of. str in python3 is
-more or less what unicode was in python2.
+unicode has been removed in Python 3. Well, again, sort of. str in Python 3 is
+more or less what unicode was in Python 2.
 
-reduce has been moved to the functools module in python3
+reduce has been moved to the functools module in Python 3
 """
-from __future__ import unicode_literals, print_function, absolute_import, division
+from __future__ import print_function, absolute_import
 
-__all__ = ('ascii', 'filter', 'hex', 'map', 'oct', 'zip',
+__all__ = ('ascii', 'filter', 'hex', 'map', 'oct', 'zip', 'input',
            'bytes', 'str', 'unicode', 'basestring', 'range', 'xrange',
            'reduce')
 
@@ -31,33 +36,41 @@ import sys
 import warnings
 import functools
 
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
 
 def py3_warn(f, name=None):
+    """Wrap callables with a deprecation warning."""
     name = name if name is not None else f.__name__
 
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
         warnings.warn('The function/class "{0}" is removed in Python 3 and '
                       'should no longer be used.'.format(name),
-                      DeprecationWarning)
+                      DeprecationWarning, stacklevel=2)
         return f(*args, **kwargs)
     return wrapper
 
-if sys.version_info[0] < 3:
+if PY2:
     from future_builtins import ascii, filter, hex, map, oct, zip
     bytes = str
     str = unicode
-    unicode = py3_warn(unicode, 'unicode')
     basestring = basestring
     range = xrange
-    xrange = py3_warn(xrange, 'xrange')
-    reduce = py3_warn(functools.reduce, 'reduce')
+    input = raw_input
 else:
     from builtins import ascii, filter, hex, map, oct, zip
     bytes = bytes
     str = str
-    unicode = py3_warn(str, 'unicode')
     basestring = (str, bytes)
     range = range
-    xrange = py3_warn(range, 'xrange')
-    reduce = py3_warn(functools.reduce, 'reduce')
+    input = input
+
+# code for both versions of Python
+# These work for both since we have already redifined these callables to match
+# Python 3.
+unicode = py3_warn(str, 'unicode')
+xrange = py3_warn(range, 'xrange')
+reduce = py3_warn(functools.reduce, 'reduce')
+raw_input = py3_warn(input, 'raw_input')

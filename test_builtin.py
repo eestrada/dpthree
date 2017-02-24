0fe47945a48e457acd85792843caaaefc49e1da8
Ethan Estrada <ethan.estrada@gmail.com>
2017-02-24 00:24:15 -0700

    Add missing `WarningsRecorder` class from internal `test.support` module

--- test_builtin.py
+++ test_builtin.py
@@ -26,6 +26,28 @@ except Exception:
 
     run_unittest = None
 
+    class WarningsRecorder(object):
+        """Convenience wrapper for the warnings list returned on
+        entry to the warnings.catch_warnings() context manager.
+        """
+        def __init__(self, warnings_list):
+            self._warnings = warnings_list
+            self._last = 0
+
+        def __getattr__(self, attr):
+            if len(self._warnings) > self._last:
+                return getattr(self._warnings[-1], attr)
+            elif attr in warnings.WarningMessage._WARNING_DETAILS:
+                return None
+            raise AttributeError("%r has no attribute %r" % (self, attr))
+
+        @property
+        def warnings(self):
+            return self._warnings[self._last:]
+
+        def reset(self):
+            self._last = len(self._warnings)
+
     def _filterwarnings(filters, quiet=False):
         """Catch the warnings, then check if all the expected
         warnings have been raised and re-raise unexpected warnings.
